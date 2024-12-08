ebdb0b4f1ce9873410e6e399b3d66ef1a4520e0f
Muhammad Ghufran <ghufranshiraz29@gmail.com>
2024-12-09 01:14:19 +0500

    first commit

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -55,7 +55,7 @@ const page = () => {
       <div className="bg-[linear-gradient(rgba(255,255,255,.1),rgba(255,255,243,.1)),url('/Rectangle-1.png')] bg-center bg-no-repeat bg-cover text-center min-h-[300px] flex items-center justify-center mb-8">
         <div className="container mx-auto text-center py-5">
           <Link href="/" className="flex items-center justify-center">
-            <Image src="/logo.png" alt="Logo" width={80} height={80} />
+            <Image src="/Logo.png" alt="Logo" width={80} height={80} />
           </Link>
           <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold mb-4 ">
             Shop
